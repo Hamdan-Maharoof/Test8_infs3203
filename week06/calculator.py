fbba741fd87823ca1b27ceb44728e70bf5de9353
hamdan <hamdanbm.19@gmail.com>
2024-02-08 20:29:46 +0300

    enchancement code for square root operation

--- week06/calculator.py
+++ week06/calculator.py
@@ -26,27 +26,30 @@ class Calculator:
 def main():
     calc = Calculator()
 
-    x = float(input("Enter first number: "))
-    y = float(input("Enter second number: "))
     operation = input("Enter operation (add, subtract, multiply, divide, modulo, power, square_root): ")
 
-    if operation == "add":
-        print(calc.add(x, y))
-    elif operation == "subtract":
-        print(calc.subtract(x, y))
-    elif operation == "multiply":
-        print(calc.multiply(x, y))
-    elif operation == "divide":
-        print(calc.divide(x, y))
-    elif operation == "modulo":
-        print(calc.modulo(x, y))
-    elif operation == "power":
-        print(calc.power(x, y))
-    elif operation == "square_root":
+    if operation == "square_root":
+        x = float(input("Enter number: "))
         print(calc.square_root(x))
+    elif operation in ["add", "subtract", "multiply", "divide", "modulo", "power"]:
+        x = float(input("Enter first number: "))
+        y = float(input("Enter second number: "))
+        if operation == "add":
+            print(calc.add(x, y))
+        elif operation == "subtract":
+            print(calc.subtract(x, y))
+        elif operation == "multiply":
+            print(calc.multiply(x, y))
+        elif operation == "divide":
+            print(calc.divide(x, y))
+        elif operation == "modulo":
+            print(calc.modulo(x, y))
+        elif operation == "power":
+            print(calc.power(x, y))
     else:
         print("Invalid operation")
 
 
 if __name__ == "__main__":
     main()
+
